5dbc010fe37f99f32dbc60de58661c52b74e39c8
Marjorie Joseph <marjo_joseph86@hotmail.com>
2021-02-17 16:11:19 -0500

    add seed and controller

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,6 +2,6 @@ class Restaurant < ApplicationRecord
   validates :name, presence: true
   validates :address, presence: true
   validates :phone_number, presence: true
-  validates :category, presence: true, inclusion:{in:["chinese", "italian", "japanese", "french", "belgian"]}
+  validates :category, presence: true, inclusion:{ in:["chinese", "italian", "japanese", "french", "belgian"]}
   has_many :reviews, dependent: :destroy
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.destroy_all
+
+poule = { name: "Ma poule mouille", address: "969 Rachel St E, Montreal", phone_number: 111, category: "french" }
+alfa = { name: "Alfa", address: "2497 Chambly Rd, Longueuil", phone_number: 222, category: "french" }
+belle = { name: "la belle et la boeuf", address: "1165, Rue Volta, Boucherville", phone_number: 333, category: "french" }
+twisted = { name: "Twisted burger", address: "205 Rue Saint-Louis, LeMoyne", phone_number: 444, category: "french" }
+seoul = { name: "seoul chako", address: "1824 Saint-Catherine St W, Montreal", phone_number: 555, category: "chinese" }
+
+[ poule, alfa, belle, twisted, seoul ].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
